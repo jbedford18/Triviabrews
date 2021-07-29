98966fbde1bb66bde5572368f7a86183d2535029
Caroline <Cluce059@gmail.com>
2021-07-28 20:25:48 -0400

    start-button

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var startButton = document.getElementById("start-btn");
+var startPage = document.getElementById("start-page");
+//function to get trivia category 
+function fetchQuestion(){
+    var apiUrl="https://opentdb.com/api.php?amount=10";
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                console.log(data.results[0]);
+                //
+                console.log(data.results[0].category);
+                var category = data.results[0].category;
+                displayCategories(category);
+            });
+        }
+        else{
+            alert("Error" + response.statusText);
+        }
+    });
+};
+fetchQuestion();
+//function to display categories on main page
+function displayCategories(category){
+    var categoryEl = document.getElementById("categories");    
+    categoryEl.textContent = category;
+};
+
+// function hideStart(){
+//     startPage.classList.add("hidden");
+//     console.log("hidden");
+// };
+startButton.onclick = () =>{
+    startButton.classList.add(hidden);
+    console.log("hidden");
+}
+///////////////////////////event listeners///////////////////////////
+//startButton.addEventListener("click", hideStart);
+
+
